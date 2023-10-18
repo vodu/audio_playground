6ac8621cbba4e69749759156979b8bd9152a9340
Volker Dümke <volker@mimi.io>
2023-10-18 23:13:09 +0200

    some simple plotting

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-// #include <cstdint>
 
 #include "AudioFile.h"
+#include "plot.h"
 
 using namespace std;
 
@@ -28,6 +28,25 @@ int main(int argc, char *argv[])
     cout << "Length in seconds: " << audioFile.getLengthInSeconds() << "\n";
 
     // plot audio file
+    
+    // Set up a plot
+
+    signalsmith::plot::Plot2D plot;
+
+    // Customise the axes
+    float numSamplesShow = 256; //audioFile.getNumSamplesPerChannel();
+    plot.x.major(0).minors(0, numSamplesShow).label("time");
+    plot.y.major(0).minors(-1, 1).label("amplitude");
+
+    // Add some data with `.add(x, y)`
+
+    auto &signal = plot.line();
+    for (int x = 0; x < numSamplesShow; x++) {
+        
+        signal.add(x, audioFile.samples[0][x]);
+    }
+
+    plot.write("default-2d.svg");
 
     return 0;
 }
