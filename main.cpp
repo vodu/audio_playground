502347d5d030657b56d582aa1c83e5947211026c
Volker Dümke <volker@mimi.io>
2023-10-25 12:26:48 +0200

    cleanup

--- main.cpp
+++ main.cpp
@@ -13,7 +13,6 @@ int main(int argc, char *argv[])
     
     // read in audio file
     const string pathToAudioFile = string(PROJECT_ROOT_DIR) + "/wav_files/wnoise.wav";
-
     AudioFile<float> audioFile;
     bool loaded = audioFile.load(pathToAudioFile);
     if (!loaded) {
@@ -21,32 +20,30 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    cout << "--- audio file info ---" << "\n";
     cout << "Audio file loaded: " << pathToAudioFile << "\n";
     cout << "Sample rate: " << audioFile.getSampleRate() << "\n";
     cout << "Bit depth: " << audioFile.getBitDepth() << "\n";
     cout << "Number of channels: " << audioFile.getNumChannels() << "\n";
     cout << "Length in seconds: " << audioFile.getLengthInSeconds() << "\n";
+    cout << "Number of samples: " << audioFile.getNumSamplesPerChannel() << "\n";
+    cout << "-----------------------" << "\n";
 
     // plot audio file
-    
-    // Set up a plot
-
     signalsmith::plot::Plot2D plot;
 
     // Customise the axes
-    float numSamplesShow = 256; //audioFile.getNumSamplesPerChannel();
-    plot.x.major(0).minors(0, numSamplesShow).label("time");
+    float numberOfSamples = audioFile.getNumSamplesPerChannel();
+    plot.x.major(0).minors(0, numberOfSamples).label("time");
     plot.y.major(0).minors(-1, 1).label("amplitude");
 
-    // Add some data with `.add(x, y)`
-
     auto &signal = plot.line();
-    for (int x = 0; x < numSamplesShow; x++) {
-        
+    for (int x = 0; x < numberOfSamples; x++) {
         signal.add(x, audioFile.samples[0][x]);
     }
 
-    plot.write("default-2d.svg");
+    plot.write("wnoise_plot.svg");
+
 
     return 0;
 }
